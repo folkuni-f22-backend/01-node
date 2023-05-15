5cf6d4c415f09d985b08ac433da4cca0f5148fbb
David Andersson <lejonmanen@gmail.com>
2023-05-15 11:40:33 +0200

    Added a question

--- start.js
+++ start.js
@@ -1,1 +1,20 @@
-console.log('Hello world!')
+import { question } from 'readline-sync'
+
+console.log('Welcome user!')
+let name = question('What is your name? ')
+
+console.log(`Welcome again, ${name}!`)
+
+/*
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
+// Handle the secret text (e.g. password).
+var favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+});
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+*/
